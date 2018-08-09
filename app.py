0a9b2452167993047878d56844450e48468c1939
Navon Francis <navonfrancis@gmail.com>
2018-08-09 19:37:46 -0400

    removed files

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Blueprint
-from flask_restful import Api
-from resources.Garages import Garages
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 
-api_bp = Blueprint('api', __name__)
-api = Api(api_bp)
+import scraper
 
-# Route
-api.add_resource(Garages, '/all')
+app = Flask(__name__)
+api = Api(app)
+
+class Garage(Resource):
+
+    def get(self):
+        return scraper.get_avail(), 200
+
+api.add_resource(Garage, "/all")
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
--- resources/Garages.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Resource
-from resources.scraper import get_avail
-
-class Garages(Resource):
-    def get(self):
-        return get_avail()
